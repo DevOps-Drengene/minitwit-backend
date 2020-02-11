89619fd9d2eab3f376dde27d7c58e22e733446a5
Caspar Olesen <caspar@skarp.dk>
2020-02-11 15:26:40 +0100

    Add database wrapper to use async/await

---
+++ src/helpers/db.js
@@ -0,0 +1,52 @@
+const sqlite3 = require('sqlite3');
+
+class DatabaseHelper {
+  constructor() {
+    this.db = new sqlite3.Database('/tmp/minitwit.db', (err) => {
+      if (err) {
+        console.error(err.message)
+      }
+      console.log('Connected to the minitwit database.')
+    });
+  }
+
+  get(sql, params) {
+    return new Promise((resolve, reject) => {
+      this.db.get(sql, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(rows);
+      });
+    });
+  }
+
+  all(sql, params) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(rows);
+      });
+    });
+  }
+
+  run(sql, params) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(rows);
+      });
+    });
+  }
+}
+
+const db = new DatabaseHelper();
+
+module.exports = db;
